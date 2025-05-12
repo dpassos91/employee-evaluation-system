5780e2301482cf13c0739aa8771b9a328c79ab67
Diogo Passos <diogopassos91@gmail.com>
2025-05-12 16:45:22 +0100

    criação de backend

---
+++ backend/src/main/java/aor/projeto/grupo7/HelloApplication.java
@@ -0,0 +1,9 @@
+package aor.projeto.grupo7;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
