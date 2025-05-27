d52ca0ec4b1197c7f642d1bd9c8fd78a37923e9c
Diogo Passos <diogopassos91@gmail.com>
2025-05-27 16:15:40 +0100

    prototipagem

---
+++ backend/src/main/java/aor/projetofinal/entity/PermissionEntity.java
@@ -0,0 +1,57 @@
+package aor.projetofinal.entity;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "permissions")
+public class PermissionEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "action", nullable = false, unique = true)
+    private String action;
+
+    @OneToMany(mappedBy = "permission")
+    private List<RolePermissionEntity> rolePermissions;
+
+    public PermissionEntity() {}
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAction() {
+        return action;
+    }
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public List<RolePermissionEntity> getRolePermissions() {
+        return rolePermissions;
+    }
+    public void setRolePermissions(List<RolePermissionEntity> rolePermissions) {
+        this.rolePermissions = rolePermissions;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PermissionEntity)) return false;
+        PermissionEntity that = (PermissionEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
