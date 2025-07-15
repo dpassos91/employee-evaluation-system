7a115aa986ae9f6832efcfe1d9cdb771781f6351
David Coelho <davidjccoelho@gmail.com>
2025-07-15 18:25:12 +0100

    BE + FE | evaluation form page disabeld state and buttons resolvido

--- backend/src/main/java/aor/projetofinal/entity/enums/EvaluationStateEnum.java
+++ backend/src/main/java/aor/projetofinal/entity/enums/EvaluationStateEnum.java
@@ -3,5 +3,13 @@ package aor.projetofinal.entity.enums;
 public enum EvaluationStateEnum {
     IN_EVALUATION,
     EVALUATED,
-    CLOSED
+    CLOSED,;
+
+
+    public static String transformToString(EvaluationStateEnum state) {
+        if (state == null) throw new IllegalArgumentException("UsualWorkPlaceType cannot be null.");
+        return state.name();
+    }
+
+
 }
